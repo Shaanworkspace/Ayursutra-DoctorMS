babca99e599ecd2b634e6eb47d00f8325c523017
shaan <shaanyworkspace@gmail.com>
2026-02-02 15:08:16 +0530

    Improvements for deployment

--- src/main/java/com/doctorms/Configuration/SecurityConfig.java
+++ src/main/java/com/doctorms/Configuration/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.doctorms.Configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -16,6 +17,7 @@ public class SecurityConfig {
 
 		http
 				.csrf(AbstractHttpConfigurer::disable)
+				.cors(Customizer.withDefaults())
 				.authorizeHttpRequests(auth -> auth
 						.requestMatchers("/**").permitAll()
 						.anyRequest().authenticated()
